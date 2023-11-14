aa6bd5e1324e72e5883d4c5d9d59e0a23311ac5e
CodingTimeYog <yogeshchess77@gmail.com>
2023-11-14 16:16:49 -0500

    first commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,27 @@
+package frc.robot;
+public class Constants {
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 6; // Check front left module drive motor ID (Neo)
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 8; // Check front left module steer motor ID (Falcon)
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER= 11; // Check front left steer encoder ID
+    // public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(113.4); // FIXME Measure and set front left steer offset
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(110.2); // FIXME Measure and set front left steer offset
+
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 4; // Check front right drive motor ID (Neo)
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 9; // Check front right steer motor ID (Falcon)
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 12; // Check front right steer encoder ID
+    //public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(68.8); // FIXME Measure and set front right steer offset
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(62.7); // FIXME Measure and set front right steer offset
+
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 2; // Check back left drive motor ID (Neo)
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 7; // Check back left steer motor ID (Falcon)
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 10; // Check back left steer encoder ID
+    // public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(229.6); // FIXME Measure and set back left steer offset
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(47.5); // FIXME Measure and set back left steer offset
+
+
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 5; // Check back right drive motor ID (Neo)
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 3; // Check back right steer motor ID(Falcon)
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 13; // Check back right steer encoder ID
+    // public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(207.4); // FIXME Measure and set back right steer offset
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(205.8); // FIXME Measure and set back right steer offset
+}
